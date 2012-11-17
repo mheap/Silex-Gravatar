05aa19211ed538fb22cd805dae3be26d74cf281b
Michael Heap <m@michaelheap.com>
2012-11-17 21:08:08 +0000

    Cache working, added tests

--- src/SilexGravatar/GravatarExtension.php
+++ src/SilexGravatar/GravatarExtension.php
@@ -13,18 +13,13 @@ use Gravatar\Service,
 
 class GravatarExtension implements ServiceProviderInterface
 {
-    public function boot(Application $app)
+    public function register(Application $app)
     {
 
     }
 
-    public function register(Application $app)
+    public function boot(Application $app)
     {
-        $app['gravatar'] = $app->share(function () use ($app) {
-            $options = isset($app['gravatar.options']) ? $app['gravatar.options'] : array();
-            return new Service($options, $app['gravatar.cache']);
-        });
-
         $app['gravatar.cache'] = $app->share(function () use ($app) {
             $cache = null;
             if(isset($app['gravatar.cache_dir'])) {
@@ -33,6 +28,11 @@ class GravatarExtension implements ServiceProviderInterface
                 $cache = new ExpiringCache($file, $ttl);
             }
             return $cache;
+        });
+
+        $app['gravatar'] = $app->share(function () use ($app) {
+            $options = isset($app['gravatar.options']) ? $app['gravatar.options'] : array();
+            return new Service($options, $app['gravatar.cache']);
         });
 
         if (isset($app['twig'])) {
--- tests/SilexGravatar/Tests/GravatarExtensionTest.php
+++ tests/SilexGravatar/Tests/GravatarExtensionTest.php
@@ -18,7 +18,7 @@ class GravatarExtensionTest extends \PHPUnit_Framework_TestCase
             $this->markTestSkipped('Gravatar was not installed.');
         }
     }
-    
+
     public function testRegister()
     {
         $app = new Application();
@@ -29,25 +29,49 @@ class GravatarExtensionTest extends \PHPUnit_Framework_TestCase
                 'default' => 'mm'
             )
         ));
-            
+
         $app->get('/', function() use($app) {
             $app['gravatar'];    
         });
         $request = Request::create('/');
         $app->handle($request);
-        
+
         $this->assertInstanceOf('Gravatar\Service', $app['gravatar']);
         $this->assertTrue($app['gravatar']->exist('sven.eisenschmidt@gmail.com'));
-        
+
         $url = $app['gravatar']->get('sven.eisenschmidt@gmail.com', array(
             'size' => 666,
             'default' => 'monsterid',
             'secure'  => true
         ));
-            
+
         $this->assertContains('https://', $url);
         $this->assertContains('r=pg', $url);
         $this->assertContains('s=666', $url);
         $this->assertContains('d=monsterid', $url);
+    }
+
+    public function testCache()
+    {
+        $app = new Application();
+        $app->register(new GravatarExtension(), array(
+            'gravatar.cache_dir'  => '/tmp/gravatar',
+            'gravatar.cache_ttl'  => 500,
+            'gravatar.options' => array(
+                'size' => 100
+            )
+        ));
+
+        // Force registering
+        $app->get("/", function() use ($app){});
+
+        $request = Request::create("/");
+        $app->handle($request);
+
+        $url = $app['gravatar']->exist('m@michaelheap.com');
+
+        $this->assertFileExists("/tmp/gravatar/10838f67e1bc005832d48ec9ed42a8b8e98e0699");
+        $this->assertFileExists("/tmp/gravatar/10838f67e1bc005832d48ec9ed42a8b8e98e0699.expires");
+
     }
 }
